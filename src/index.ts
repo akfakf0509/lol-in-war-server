6e5a2763c2286ab7e78c7e968ccc276f0c8720c1
FLY123 <akfakf0509@naver.com>
2024-01-16 04:44:24 +0900

    feat: socket io 만 사용

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
-import { App } from "uWebSockets.js";
 import { Server } from "socket.io";
 
-const app = App();
 const io = new Server();
-
-io.attachApp(app);
 
 io.on("connection", (socket) => {
   console.log("New connection");
 });
 
-app.listen(3001, (token) => {
-  if (!token) console.warn("Failed to listen to port 3001");
-  console.log("Listening to port 3001");
-});
+io.listen(3001);
